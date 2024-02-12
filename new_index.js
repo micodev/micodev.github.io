1866929932fcd5d1216097c71054e8f646120cbc
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2024-02-12 12:09:39 +0300

    modified:   new_index.js

--- new_index.js
+++ new_index.js
@@ -163,6 +163,7 @@ for (var i = 0; i < gridNum; i++) {
       alert("Game Over");
       for (var i = 0; i < blocks.length; i++) {
         if (blocks[i].isBomb) {
+          blocks[i].isChecked = true;
           blockIcon = $("<i class='fas fa-bomb fa-lg'></i>");
           var bombElement = $("[data-index=" + i + "]");
           bombElement.append(blockIcon);
